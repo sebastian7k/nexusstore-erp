11d053257056c04ed099f40b007d92210efc3078
Delane Mirande <delanemaia@gmail.com>
2026-05-26 13:30:54 -0300

    Api funcionando e 100 on

--- nexusstore-api/server.js
+++ nexusstore-api/server.js
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false
 });
 
 function validarCampos(body, campos) {
